6208a8ba3d519066247135f21470042b224b4201
vigneshwara044 <vigneshwara044@gmail.com>
2019-03-22 18:07:38 +0530

    initial publish

---
+++ precache-manifest.49d37ffe4dbf2bf401f6295575f17443.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b6aa34129382b9b47fee",
+    "url": "/static/js/main.a8a43bfd.chunk.js"
+  },
+  {
+    "revision": "2e5a4a1f5116a02e1969",
+    "url": "/static/js/2.aeafaad3.chunk.js"
+  },
+  {
+    "revision": "b6aa34129382b9b47fee",
+    "url": "/static/css/main.fd871baf.chunk.css"
+  },
+  {
+    "revision": "0dc01165578c356fa339b135b8c8d55f",
+    "url": "/index.html"
+  }
+];
